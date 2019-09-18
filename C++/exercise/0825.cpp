4d550083db6b165041bcaf9c26ef6dc4975428a2
COTI <2841267531@qq.com>
2019-09-18 08:24:16 +0800

    命名空间

--- C++/exercise/0825.cpp
+++ C++/exercise/0825.cpp
@@ -3,11 +3,12 @@
 
 using namespace std;//std域对自己可见
 
+//命名空间内可以包含其他的（一个或多个）命名空间
 namespace test {
 	namespace n1 {
 		int add(int a, int b)
 		{
-			std::cout << "1\n";
+			std::cout << "1\n";//若开头写了std，则输出可以不写
 			return a + b;
 		}
 
@@ -30,15 +31,15 @@ namespace n2 {
 	}
 }
 
-void func(int a , int b ,int c ); //缺省参数不能在函数声明和定义中同时出现
-void func(int a = 10,int b=20,int c=30)//全缺省参数
+void func(int a, int b, int c); //缺省参数不能在函数声明和定义中同时出现
+void func(int a = 10, int b = 20, int c = 30)//全缺省参数   半缺省参数必须按顺序赋值，即不能在调用时，写(func(10, ,30)
 {
 	cout << a << endl;
 	cout << b << endl;
 	cout << c << endl;
 }
 
-void swapArgs(int *pa, int *pb)
+void swapArgs(int *pa, int *pb)//参数不同，函数不同
 {
 	int tmp;
 	tmp = *pa;
@@ -66,8 +67,8 @@ int main()
 	int c = 4, d = 9;
 	swapArgs(&a, &b);
 	swapArgs(&c, &d);
-	cout << a << " "<<b<<endl;
-	cout << c << " " <<d<< endl;
+	cout << a << " " << b << endl;
+	cout << c << " " << d << endl;
 
 	system("pause");
 	return 0;
